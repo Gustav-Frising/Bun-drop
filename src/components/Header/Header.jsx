efcead432199094272ab14c9cf64ff9498a092b8
Gustav-Frising <frising96@gmail.com>
2024-06-10 01:03:13 +0200

    Navbar-header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./Header.css";
 function Header() {
-  return <></>;
+  return (
+    <>
+      <div className="header">
+        <div className="header-content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+            expedita alias quasi asperiores. Cum, sed reiciendis adipisci sit
+            voluptatibus vel.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Header;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,28 @@ function Navbar() {
  
   return (
     <>
+      <nav className={`navbar ${isHomePage ? "home-navbar" : "navbar"}`}>
+        <div className="navbar-left">
+          <ul className="navbar-links">
+            <Link to={"/"}>
+              <li className="home">Home</li>
+            </Link>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="logo">
+          <img src="/Bun_drop_logo.png" alt="image" />
+        </div>
+        <div className="navbar-right">
+          
+            <button onClick={handleLogout}>Logout</button>        
+          <Link to={"/cart"}>
+            <button>Cart</button>
+          </Link>
+        </div>
+      </nav>
     </>
   );
 }
